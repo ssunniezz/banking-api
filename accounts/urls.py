4f73190fee47e947181535f82144ca36c2becf39
ssunniezz <ssunniez@gmail.com>
2024-07-26 00:01:01 +0700

    Edit url path

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from .views import AccountViewSet, TransactionViewSet, CustomAccountViewSet
 router = DefaultRouter()
 router.register(r'accounts', AccountViewSet)
 router.register(r'transactions', TransactionViewSet)
-router.register(r'custom_accounts', CustomAccountViewSet, basename='custom_account')
+router.register(r'accounts', CustomAccountViewSet, basename='custom_account')
 
 urlpatterns = [
     path('', include(router.urls)),
